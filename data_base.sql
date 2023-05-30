836c58721cf53ca9f7678c3c61c9f9246f4c9a18
josesito <josesito@gmail.com>
2023-05-30 13:12:36 -0500

    ..

--- data_base.sql
+++ data_base.sql
@@ -9,7 +9,7 @@ CREATE TABLE  categorias(
     imagen MEDIUMBLOB
 );
 
-CREATE TABLE clientes(
+/* CREATE TABLE clientes(
     cliente_id INT primary key AUTO_INCREMENT,
     celular VARCHAR (50),
     compañia VARCHAR (50) 
@@ -57,4 +57,4 @@ CREATE TABLE proveedores (
     nombre VARCHAR(60),
     telefono INT,
     ciudad VARCHAR(60)
-);
+); */
