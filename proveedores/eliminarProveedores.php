b231f16c4393e951ee4bb2554898ac0838502bfc
JoseK03 <dajoseva@gmail.com>
2023-06-02 15:11:26 -0500

    eliminar proveedores

--- proveedores/eliminarProveedores.php
+++ proveedores/eliminarProveedores.php
@@ -8,13 +8,12 @@ error_reporting(E_ALL);
 require_once("config.php");
 $config = new Config();
 
-if(isset($_GET['proveedor_id'])&& isset($_GET['req'])){
+if(isset($_GET['proveedor_id'])&& isset($_GET['req']) ){
     if($_GET['req']=='delete'){
         $config->SetProveedorId($_GET['proveedor_id']);
         $config->Delete();
-
+        
         echo "<script>alert('Los datos se han borrado satisfactoriamente');document.location='proveedores.php'</script>";
-
     }
 }
 
